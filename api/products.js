e9a8949e5269ebc6f1745701891ccf7ee39e706e
choehyeog <orange88@naver.com>
2026-04-01 18:58:23 +0900

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,7 +1,7 @@
 // api/products.js
 export default async function handler(req, res) {
   // [필독] 현재 사용 중인 토큰을 여기에 꼭 넣으세요!
-  const accessToken = 'RM52dyekIy4J3uT2TiWjPH'; 
+  const accessToken = 'rnAjsiq4JNdiMyKqinepfN'; 
   const mallId = 'myfootsize';
 
   const page = parseInt(req.query.page) || 1;
